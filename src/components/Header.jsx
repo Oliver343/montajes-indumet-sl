a3597d15d961c36ffd3b83aa6be070e478c6950a
Oliver343 <oliver_343@hotmail.com>
2024-03-25 13:56:08 +0100

    scroll links added to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 import logo from '../img/logo.webp'
 
-export default function Header({handleGallery, handleHome}) {
+export default function Header({handleGallery, handleHome, handleAbout}) {
     return (
         <div className="header">
             <div className='inner-head'>
@@ -9,7 +9,7 @@ export default function Header({handleGallery, handleHome}) {
             </div>
             <div className='inner-head'>
                 <button className='header-link' onClick={handleHome} >HOME</button>
-                <button className='header-link'>ABOUT</button>
+                <button className='header-link' onClick={handleAbout}>ABOUT</button>
                 <button className='header-link' onClick={handleGallery} >GALLERY</button>
             </div>
         </div>
