e7ae9c6a00f41abc05c20a641b4e53b27bad7e77
Oliver Sanderson <oliver_343@hotmail.com>
2024-03-27 12:22:29 +0100

    added title to iframe to enable build

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -18,7 +18,7 @@ export default function Contact() {
             </div>
 
             <div>
-            <iframe width="400" height="300" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" id="gmap_canvas" src="https://maps.google.com/maps?width=400&amp;height=300&amp;hl=en&amp;q=Landaluc%C3%ADa%20Vitoria-Gasteiz+(montajes-indumet-sl)&amp;t=&amp;z=15&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe> <script type='text/javascript' src='https://embedmaps.com/google-maps-authorization/script.js?id=ef35c703e5ba04327757352479d7c6a69f16daa1'></script>
+                <iframe title="mapFrameItem" width="400" height="300" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" id="gmap_canvas" src="https://maps.google.com/maps?width=400&amp;height=300&amp;hl=en&amp;q=Landaluc%C3%ADa%20Vitoria-Gasteiz+(montajes-indumet-sl)&amp;t=&amp;z=15&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe> <script type='text/javascript' src='https://embedmaps.com/google-maps-authorization/script.js?id=ef35c703e5ba04327757352479d7c6a69f16daa1'></script>
             </div>
 
             <div><img src={logo} alt=""/></div>
